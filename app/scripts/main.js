4705e956082fb4ef18dead2df1bcd4b2008ae161
heshamabdalla <shamoh702@gmail.com>
2016-06-30 10:11:26 -0700

    Added layout to the page with images and paragraphs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,3 @@
-$(document).ready(function(){
-
-
-
-
-
     function dropHouse () {
   $('.moon').append('<img class=\'house\' src=\'http://herndonrentals.com/wp-content/uploads/sites/295/2014/05/house.png\' />');
 }
@@ -13,40 +7,57 @@ $(document).ready(function(){
 
 
 
-// //More fire:
-// var obj = $('#ire');
-// var fps = 200;
-// var letters = obj.html().split('');
-// obj.empty();
-// $.each(letters,function(el){
-//     obj.append($('<span>'+this+'</span>'));
-// });
-// var animateLetters = obj.find('span');
-// setInterval(function(){
-//     animateLetters.each(function(){
-//         $(this).css('fontSize', 80+(Math.floor(Math.random()*50)));
-//     });
-// },fps);
+//More fire:
+var obj = $('#ire');
+var fps = 200;
+var letters = obj.html().split('');
+obj.empty();
+$.each(letters,function(el){
+    obj.append($('<span>'+this+'</span>'));
+});
+var animateLetters = obj.find('span');
+setInterval(function(){
+    animateLetters.each(function(){
+        $(this).css('fontSize', 80+(Math.floor(Math.random()*50)));
+    });
+},fps);
 
 
 
+$(document)
+.ready(function() {
+    //More fire:
+    var obj = $('#ire');
+    var fps = 200;
+    var letters = obj.html().split('');
+    obj.empty();
+    $.each(letters,function(el){
+        obj.append($('<span>'+this+'</span>'));
+    });
+    var animateLetters = obj.find('span');
+    setInterval(function(){
+        animateLetters.each(function(){
+            $(this).css('fontSize', 80+(Math.floor(Math.random()*50)));
+        });
+    },fps);
 
-// fix menu when passed
+    // fix menu when passed
     $('.masthead')
-      .visibility({
+    .visibility({
         once: false,
         onBottomPassed: function() {
-          $('.fixed.menu').transition('fade in');
+            $('.fixed.menu').transition('fade in');
         },
         onBottomPassedReverse: function() {
-          $('.fixed.menu').transition('fade out');
+            $('.fixed.menu').transition('fade out');
         }
-      })
+    })
     ;
 
     // create sidebar and attach to menu open
     $('.ui.sidebar')
-      .sidebar('attach events', '.toc.item')
+    .sidebar('attach events', '.toc.item')
     ;
 
-});
+})
+;
