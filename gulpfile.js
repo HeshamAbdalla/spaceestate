4abf55f904b62d0b916588276e2a91cdb0fa13ad
heshamabdalla <shamoh702@gmail.com>
2016-07-05 18:27:55 -0700

    Added gulp build task for videos

--- gulpfile.js
+++ gulpfile.js
@@ -175,7 +175,7 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+gulp.task('build', ['lint', 'html', 'images', 'videos','fonts', 'extras'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
