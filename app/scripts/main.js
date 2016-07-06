aaae2f8fdab45d120a808eec622f3c82278dd392
heshamabdalla <shamoh702@gmail.com>
2016-07-06 11:14:58 -0700

    Implemented form with calc for houses prices and fixed minor bugs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -93,14 +93,140 @@ $(document).ready(function() {
 });
 
 function updateForm() {
-    var raw = $('form').serializeArray();
-    var data = [];
-    for(var key in raw) {
-        if (raw[key]['value'] !== '') {
-            data.push(raw[key]['value']);
-        }
+  var form = $('form').form('get values'),
+      total = 0;
+  for(var key in form) {
+    if(key === 'bedroom') {
+      switch(form[key]) {
+        case '1':
+          total += 1.0;
+          break;
+        case '2':
+          total += 2.5;
+          break;
+        case '3':
+          total += 3.7;
+          break;
+        case '4':
+          total += 6.2;
+          break;
+        case '5':
+          total += 7.8;
+          break;
+        case '6':
+          total += 9.2;
+          break;
+      }
     }
-    console.log(data);
+    if(key === 'bathroom') {
+      switch(form[key]) {
+        case '1':
+          total += 0.5;
+          break;
+        case '2':
+          total += 0.8;
+          break;
+        case '3':
+          total += 1.2;
+          break;
+        case '4':
+          total += 3.8;
+          break;
+        case '5':
+          total += 5.5;
+          break;
+        case '6':
+          total += 6.2;
+          break;
+      }
+    }
+    if(key === 'squarefootage') {
+      switch(form[key]) {
+        case '1000':
+          total += 10.2;
+          break;
+        case '2000':
+          total += 15.3;
+          break;
+        case '3000':
+          total += 22.4;
+          break;
+        case '4000':
+          total += 35.3;
+          break;
+        case '5000':
+          total += 51.2;
+          break;
+        case '6000':
+          total += 66.2;
+          break;
+      }
+    }
+    if(key === 'lottype') {
+      switch(form[key]) {
+        case 'basic':
+          total += 5.2;
+          break;
+        case 'premium':
+          total += 12.1;
+          break;
+        case 'luxurious':
+          total += 18.6;
+          break;
+        case 'historic':
+          total += 50.4;
+          break;
+        case 'custom':
+          total += 30.5;
+          break;
+      }
+    }
+    if(key === 'acreage') {
+      switch(form[key]) {
+        case '1':
+          total += 10.0;
+          break;
+        case '2':
+          total += 20.0;
+          break;
+        case '3':
+          total += 30.0;
+          break;
+        case '4':
+          total += 40.0;
+          break;
+        case '5':
+          total += 50.0;
+          break;
+        case '6':
+          total += 60.0;
+          break;
+      }
+    }
+    if(key === 'oven' && form[key] === 'on')
+      total += 5.0;
+    if(key === 'floor' && form[key] === 'on')
+      total += 7.2;
+    if(key === 'security' && form[key] === 'on')
+      total += 12.3;
+    if(key === 'gym' && form[key] === 'on')
+      total += 8.9;
+    if(key === 'wifi' && form[key] === 'on')
+      total += 1.5;
+    if(key === 'pool' && form[key] === 'on')
+      total += 17.9;
+    if(key === 'garage' && form[key] === 'on')
+      total += 3.4;
+    if(key === 'pad' && form[key] === 'on')
+      total += 20.4;
+    if(key === 'shield' && form[key] === 'on')
+      total += 33.2;
+    if(key === 'garden' && form[key] === 'on')
+      total += 0.8;
+  }
+  total = Math.floor(total * 10e6);
+  total = total.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+  $('#price').text(total);
 }
 
 
